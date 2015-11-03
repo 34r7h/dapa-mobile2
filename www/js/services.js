11360e7a1d2d8d1f2199946dc8c45a1889661117
irthos <irth03@gmail.com>
2015-11-02 22:28:16 -0800

    basic firebase integration with registration feature.

--- www/js/services.js
+++ www/js/services.js
@@ -1,3 +1,7 @@
+var fb = Firebase;
+var dapaRef = 'https://irthos.firebaseio.com/dapa';
+fbRef = new fb(dapaRef);
+
 angular.module('app.services', [])
 .service('Ui', ['$window', function ($window) {
 		var ui = {
@@ -6,10 +10,53 @@ angular.module('app.services', [])
 		};
 		return ui;
 	}])
-.service('Api', ['$ionicHistory', '$http', function($ionicHistory, $http){
+.service('Api', ['$ionicHistory', '$http', '$firebaseObject', '$firebaseArray', '$firebaseAuth', function($ionicHistory, $http, $firebaseObject, $firebaseArray, $firebaseAuth){
 		var baseUrl = 'http://api.dapa.dev:3000';
 		var api = {
+			fbTest: function (data) {
+				var fbObject = $firebaseObject(fbRef);
+				fbObject.$loaded().then(function () {
+					fbObject['test'] = data;
+					fbObject.$save();
+				})
+			},
 			register: function (user) {
+
+				/*
+				*
+				  *  Mock User Creation using Firebase
+				*
+				* */
+
+				var fbAuth = $firebaseAuth(fbRef);
+				fbAuth.$createUser({
+					email: user.email,
+					password: user.password
+				}).then(function(userData) {
+					console.log("User " + userData.uid + " created successfully!");
+					return fbAuth.$authWithPassword({
+						email: user.email,
+						password: user.password
+					});
+				}).then(function(authData) {
+					console.log("Logged in as:", authData.uid);
+					user.password = null;
+					var fbObject = $firebaseObject(fbRef.child('users'));
+					fbObject.$loaded().then(function () {
+						fbObject[authData.uid] = user;
+						fbObject.$save();
+					})
+
+				}).catch(function(error) {
+					console.error("Error: ", error);
+				});
+				/*
+				*
+					*  End Mock DB
+				*
+				* */
+
+
 				console.log('Registering', user);
 				$http.post(baseUrl + '/users', {
 					headers:{
